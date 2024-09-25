d9273a546d1fd7ece97a99815302f99e3e6322a1
Swejda9940 <Swejda9940@saskpolytech.ca>
2024-09-25 09:51:08 -0600

    secure login stuff, making the pages n all you know

--- routes/secure.js
+++ routes/secure.js
@@ -27,7 +27,32 @@ router.post('/', function (req, res) {
     title: 'Get Login Form',
     submittedEmail: req.body.email, // Hard Coded FOR NOW
     submittedPassword: req.body.pwd, // Hard Coded FOR NOW
+    submittedData: req.body.data,
     isSubmitted: true,
     err: { email: 'Email it not recognized', pwd: 'Password is not recognized' } // Hard Coded FOR NOW
   })
 })
+
+/* Get handler for three paths - DASHBOARD, PROFILE, BOOKING */
+router.get(['/dashboard', '/profile', '/booking'], function (req, res) {
+  // determine which page the user is trying to access from the url path
+  const scope = req.path.replace(/^\/+|\/+$/g, '')
+
+  res.render('secure-generic', {
+    title: scope.toUpperCase(),
+    action: req.baseUrl + req.path // determine form submit from url and path
+  })
+})
+
+/* POST handler for all 3 generic paths */
+router.post(['/dashboard', '/profile', '/booking'], function (req, res) {
+  // determine which page the user is trying to access from the url path
+  const scope = req.path.replace(/^\/+|\/+$/g, '')
+
+  res.render('secure-generic', {
+    title: 'POST' + scope.toUpperCase()
+    // no action I will this as a boolean to determine wether or not to display the form on the page
+
+  })
+})
+module.exports = router
